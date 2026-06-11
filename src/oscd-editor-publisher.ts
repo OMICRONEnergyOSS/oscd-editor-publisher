9a1d2e6d3fbfc3b76efbd9bcb8c622ada523b6fb
Stephen Renwick <stephen.renwick@omicronenergy.com>
2026-06-11 11:16:17 +0200

    feat: migrate tooling over to @omicronenergy/oscd-tooling

--- src/oscd-editor-publisher.ts
+++ src/oscd-editor-publisher.ts
@@ -4,12 +4,13 @@ import { classMap } from 'lit/directives/class-map.js';
 
 import { ScopedElementsMixin } from '@open-wc/scoped-elements/lit-element.js';
 
-import { OscdRadio } from '@omicronenergy/oscd-ui/radio/OscdRadio.js';
-
 import { DataSetEditor } from './editors/dataset/data-set-editor.js';
 import { GseControlEditor } from './editors/gsecontrol/gse-control-editor.js';
 import { ReportControlEditor } from './editors/report/report-control-editor.js';
 import { SampledValueControlEditor } from './editors/sampledvalue/sampled-value-control-editor.js';
+import { OscdOutlinedSegmentedButton } from '@omicronenergy/oscd-ui/labs/segmentedbutton/OscdOutlinedSegmentedButton.js';
+import { OscdOutlinedSegmentedButtonSet } from '@omicronenergy/oscd-ui/labs/segmentedbuttonset/OscdOutlinedSegmentedButtonSet.js';
+import { OscdIcon } from '@omicronenergy/oscd-ui/icon/OscdIcon.js';
 
 export const PUBLISHER_TYPE_LOCAL_STORAGE_KEY =
   'oscd-editor-publisher__publisher-type';
@@ -38,7 +39,9 @@ function storedPublisherType(): PublisherType {
 /** An editor [[`plugin`]] to configure `Report`, `GOOSE`, `SampledValue` control blocks and its `DataSet` */
 export default class PublisherPlugin extends ScopedElementsMixin(LitElement) {
   static scopedElements = {
-    'oscd-radio': OscdRadio,
+    'oscd-outlined-segmented-button': OscdOutlinedSegmentedButton,
+    'oscd-outlined-segmented-button-set': OscdOutlinedSegmentedButtonSet,
+    'oscd-icon': OscdIcon,
     'report-control-editor': ReportControlEditor,
     'gse-control-editor': GseControlEditor,
     'sampled-value-control-editor': SampledValueControlEditor,
@@ -63,50 +66,41 @@ export default class PublisherPlugin extends ScopedElementsMixin(LitElement) {
 
   render() {
     return html`<form class="publishertypeselector">
-        <span>
-          <oscd-radio
-            id="report-radio"
-            value="report"
-            ?checked=${this.publisherType === 'Report'}
-            @change=${() => {
-              this.selectPublisherType('Report');
-            }}
-          ></oscd-radio>
-          <label for="report-radio">Report</label>
-        </span>
-        <span>
-          <oscd-radio
-            id="goose-radio"
-            value="goose"
-            ?checked=${this.publisherType === 'GOOSE'}
-            @change=${() => {
-              this.selectPublisherType('GOOSE');
-            }}
-          ></oscd-radio>
-          <label for="goose-radio">GOOSE</label>
-        </span>
-        <span>
-          <oscd-radio
-            id="smv-radio"
-            value="smv"
-            ?checked=${this.publisherType === 'SampledValue'}
-            @change=${() => {
-              this.selectPublisherType('SampledValue');
-            }}
-          ></oscd-radio>
-          <label for="smv-radio">SampledValue</label>
-        </span>
-        <span>
-          <oscd-radio
-            id="ds-radio"
-            value="ds"
-            ?checked=${this.publisherType === 'DataSet'}
-            @change=${() => {
-              this.selectPublisherType('DataSet');
-            }}
-          ></oscd-radio>
-          <label for="ds-radio">DataSet</label>
-        </span>
+        <oscd-outlined-segmented-button-set class="control-switch">
+          <oscd-outlined-segmented-button
+            label="GOOSE"
+            no-checkmark
+            ?selected=${this.publisherType === 'GOOSE'}
+            @click=${() => this.selectPublisherType('GOOSE')}
+          >
+            <oscd-icon slot="icon">gooseIcon</oscd-icon>
+          </oscd-outlined-segmented-button>
+          <oscd-outlined-segmented-button
+            label="SampledValue"
+            no-checkmark
+            ?selected=${this.publisherType === 'SampledValue'}
+            @click=${() => this.selectPublisherType('SampledValue')}
+          >
+            <oscd-icon slot="icon">smvIcon</oscd-icon>
+          </oscd-outlined-segmented-button>
+          <oscd-outlined-segmented-button
+            label="Report"
+            no-checkmark
+            ?selected=${this.publisherType === 'Report'}
+            @click=${() => this.selectPublisherType('Report')}
+          >
+            <oscd-icon slot="icon">reportIcon</oscd-icon>
+          </oscd-outlined-segmented-button>
+
+          <oscd-outlined-segmented-button
+            label="DataSet"
+            no-checkmark
+            ?selected=${this.publisherType === 'DataSet'}
+            @click=${() => this.selectPublisherType('DataSet')}
+          >
+            <oscd-icon slot="icon">dataSetIcon</oscd-icon>
+          </oscd-outlined-segmented-button>
+        </oscd-outlined-segmented-button-set>
       </form>
       <report-control-editor
         .doc=${this.doc}
@@ -140,16 +134,18 @@ export default class PublisherPlugin extends ScopedElementsMixin(LitElement) {
 
   static styles = css`
     * {
+      --oscd-primary: var(--oscd-theme-primary, #0b335b);
+      --oscd-secondary: var(--oscd-theme-secondary, #2485e5);
+      --oscd-base00: var(--oscd-theme-base01, #46505d);
+      --oscd-base01: var(--oscd-theme-base01, #3d4651);
+      --oscd-base2: var(--oscd-theme-base01, #f3f5f6);
+      --oscd-base3: var(--oscd-theme-base01, #FFF);
+      --oscd-font: var(--oscd-theme-font, 'Roboto');
+      font-family: var(--oscd-font);
+
       --md-sys-color-primary: var(--oscd-primary);
       --md-sys-color-secondary: var(--oscd-secondary);
-      --md-sys-typescale-body-large-font: var(--oscd-text-font);
-      --md-sys-typescale-body-medium-font: var(--oscd-text-font);
-      --md-sys-typescale-body-small-font: var(--oscd-text-font);
-      --md-sys-typescale-label-large-font: var(--oscd-text-font);
-      --md-sys-typescale-label-medium-font: var(--oscd-text-font);
-      --md-ref-typeface-plain: var(--oscd-text-font);
       --md-outlined-text-field-input-text-color: var(--oscd-base01);
-
       --md-sys-color-surface: var(--oscd-base3);
       --md-sys-color-on-surface: var(--oscd-base00);
       --md-sys-color-on-primary: var(--oscd-base2);
@@ -161,7 +157,7 @@ export default class PublisherPlugin extends ScopedElementsMixin(LitElement) {
       --md-list-item-activated-background: rgb(
         from var(--oscd-primary) r g b / 0.38
       );
-      font-family: var(--oscd-text-font);
+      font-family: var(--oscd-font);
     }
 
     .hidden {
@@ -169,16 +165,66 @@ export default class PublisherPlugin extends ScopedElementsMixin(LitElement) {
     }
 
     .publishertypeselector {
-      margin: 4px 8px 8px;
-      padding: 8px;
-      background-color: var(--oscd-base3);
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(150px, auto));
-      gap: 5px;
+      display: flex;
+      align-items: flex-start;
+      gap: 16px;
+      padding: 8px 12px;
+      flex-wrap: wrap;
+      background-color: var(--oscd-base2);
     }
 
-    .publishertypeselector > span > label {
-      margin-left: 10px;
+    oscd-outlined-segmented-button-set {
+      flex-shrink: 0;
+      align-self: flex-start;
+      inline-size: min(100%, 42rem);
+      --md-outlined-segmented-button-selected-container-color: var(
+        --md-sys-color-primary,
+        #005ac1
+      );
+      --md-outlined-segmented-button-selected-label-text-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-icon-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-hover-label-text-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-hover-icon-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-focus-label-text-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-focus-icon-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-pressed-label-text-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-pressed-icon-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-hover-state-layer-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+      --md-outlined-segmented-button-selected-pressed-state-layer-color: var(
+        --md-sys-color-on-primary,
+        #ffffff
+      );
+    }
+
+    .control-switch oscd-outlined-segmented-button {
+      min-inline-size: 0;
     }
   `;
 }
